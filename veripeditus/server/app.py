4a11b90f8d93cd1e2a4756b10f2bcd7489cd13eb
Dominik George <nik@naturalnet.de>
2017-02-16 12:22:55 +0100

    Add GCE note to dhort licence paragraph, long text to COPYING.

--- veripeditus/server/app.py
+++ veripeditus/server/app.py
@@ -10,7 +10,7 @@ This module contains everything to set up the Flask application.
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# (at your option) any later version, with the Game Cartridge Exception.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of

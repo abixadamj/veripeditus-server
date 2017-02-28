fc0c6bf9726f2c8f3e53debbff4cf036f145d5b6
Eike Jesinghaus <eike@naturalnet.de>
2017-02-28 13:39:08 +0100

    Make Location require a distance_max attribute to be passed. Advances #97.

--- veripeditus/framework/model.py
+++ veripeditus/framework/model.py
@@ -425,7 +425,7 @@ class Player(GameObject):
                 item.collect()
 
         for loc in Location.query.filter_by(world=current_player().world).all():
-            if loc.distance_to(self) < loc.distance_max:
+            if hasattr(loc, "distance_max") and loc.distance_to(self) < loc.distance_max:
                 loc.pass_()
 
         DB.session.add(self)
